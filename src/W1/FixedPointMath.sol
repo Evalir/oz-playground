ae631cfac4c735e2f23a37bc2e15846d43418347
Enrique Ortiz <hi@enriqueortiz.dev>
2023-03-15 11:27:46 -0400

    feat: diff fuzzing with solmate for fixed point math

--- src/W1/FixedPointMath.sol
+++ src/W1/FixedPointMath.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract FixedPointMath {
+library FixedPointMath {
     function mulWad(uint256 x, uint256 y) public pure returns (uint256) {
         return x * y / 1e18;
     }
--- test/FixedPointMath.t.sol
+++ test/FixedPointMath.t.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "forge-std/Test.sol";
 import "src/W1/FixedPointMath.sol";
+import "solmate/utils/FixedPointMathLib.sol";
 import "./Helpers.sol";
 
-contract FixedPointMathTest is Helpers {}
+contract FixedPointMathTest is Helpers {
+    function setUp() public {}
+
+    function test_diff_fuzz_Solmate(uint256 x, uint256 y) public view {
+        x = bound(x, 1, uint256(1*1e38));
+        y = bound(y, 1, uint256(1*1e38));
+        uint a = FixedPointMath.mulWad(x, y);
+        uint b = FixedPointMathLib.mulWadDown(x, y);
+        assert(a == b);
+    }
+}
