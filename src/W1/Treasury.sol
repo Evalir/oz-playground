aa74131c03cb04d15da63c1789fd57b112c74900
Enrique Ortiz <hi@enriqueortiz.dev>
2023-03-15 20:00:00 -0400

    feaT: trasury audit poc

---
+++ src/W1/Treasury.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+/**
+ * @title Treasury
+ * @dev This contract accepts donations in ERC-20 tokens. Only tokens that have been added to the
+ * allowlist can be donated. The allowlist can only be modified by the contract owner which is
+ * controlled by the DAO. All tokens added to the allowlist are highly liquid and trusted tokens
+ * which have been properly vetted by industry professionals. All tokens follow the ERC-20
+ * specification.
+ */
+contract Treasury is Ownable {
+
+    using SafeERC20 for ERC20;
+
+    struct Donation {
+        uint256 amount;       // Total amount of the token that has been donated
+        uint256 numDonations; // The total number of donations the account has made
+    }
+
+    // Registry that tracks donations by token and donor
+    mapping(address => mapping(address => Donation)) public donations;
+
+    // Allowlist that the owner can add tokens to. Only tokens on the allowlist can be donated.
+    mapping(address => bool) public allowlist;
+
+    /**
+     * @dev Transfers _amount of _token from the callers account to this contract and records
+     * the donation.
+     * @param _token The ERC-20 token that is being donated.
+     * @param _amount Amount of _token that is being donated.
+     */
+    function donate(address _token, uint256 _amount) external {
+
+        // Only tokens on the allowlist can be donated
+        require(allowlist[_token], "Invalid token");
+
+        // Transfer the appropriate amount of tokens from msg.sender using safeTransferFrom
+        // to ensure the call reverts if the transfer fails.
+        ERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+
+        // Update the registry with the new donation
+        _updateDonor(donations[_token][msg.sender], _amount);
+    }
+
+    function allow(address _token) external onlyOwner {
+        allowlist[_token] = true;
+    }
+
+    /**
+     * @dev Get the amount that _account has donated in _token.
+     * @param _account Account address
+     * @param _token ERC-20 token
+     */
+    function getDonation(address _account, address _token) public view returns (uint256) {
+        return donations[_account][_token].amount;
+    }
+    
+    // Update the donor registry
+    function _updateDonor(Donation memory donation, uint256 _amount) private {
+        unchecked{
+            donation.amount += _amount;
+            donation.numDonations++;
+        }
+    }
+
+    /* Additional functions that have been properly implemented and are bug-free. */
+    
+}
---
+++ test/Treasury.t.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/W1/Treasury.sol";
+import "test/Helpers.sol";
+import "test/mocks/ERC20.sol";
+
+contract TreasuryPoC is Helpers {
+    Treasury public t;
+    MockERC20 public token;
+    MockERC20 public unauthorizedToken;
+    address public owner;
+
+    function setUp() public {
+        t = new Treasury();
+        token = new MockERC20("Mock", "MCK");
+        unauthorizedToken = new MockERC20("Mock2", "MCK2");
+        token.mint(address(this), 1000);
+        unauthorizedToken.mint(address(this), 1000);
+        owner = account("owner");
+    }
+
+    /// @dev This test is the PoC for the bug found in the treasury contract,
+    /// which is that the treasury does not save donations as they're written to a in-memory variable
+    /// instead of to storage.
+    function test_donationBug() public {
+        // Approve token for transfer
+        t.allow(address(token));
+        // Owner donates 100 tokens.
+        token.approve(address(t), 100);
+        t.donate(address(token), 100);
+
+        // expect the donations to NOT change,
+        // even though the token is held by the treasury.
+        assertEq(t.getDonation(address(this), address(token)), 0);
+        assertEq(token.balanceOf(address(t)), 100);
+        assertEq(token.balanceOf(address(this)), 900);
+    }
+
+    /// @dev This test is for a bug (or unexpected behavior) that was found in the treasury contract,
+    /// which is that the treasury can receive unauthorized tokens. While not a problem in itself
+    /// (even if the accounting worked correctly), depending on the contract usage, it could be
+    /// considered a problem e.g if the contract could not legally hold OFAC restricted tokens
+    /// Or somehow did some internal accounting based on the tokens it held (in case the token is allowed).
+    function test_transferBug() public {
+        // Approve token for transfer
+        t.allow(address(token));
+        // Owner donates 100 tokens.
+        token.approve(address(t), 100);
+        t.donate(address(token), 100);
+
+        // This is not necessarily a bug, but rather a behavior to be aware of:
+        // The treasury can receive unauthorized tokens; they just won't be assigned to the donor.
+        // This can be done by just transfering to the treasury address.
+        unauthorizedToken.transfer(address(t), 100);
+        assertEq(t.getDonation(address(this), address(unauthorizedToken)), 0);
+        assertEq(t.allowlist(address(unauthorizedToken)), false);
+        assertEq(unauthorizedToken.balanceOf(address(t)), 100);
+        assertEq(unauthorizedToken.balanceOf(address(this)), 900);
+    }
+}
